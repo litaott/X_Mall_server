4a6d3d5f17755f57124415982aed262125f06de7
xyan <2624777121@qq.com>
2024-07-09 17:21:38 +0800

    接口部分1

--- src/main/java/com/little/xmall/mapper/UserInfoMapper.java
+++ src/main/java/com/little/xmall/mapper/UserInfoMapper.java
@@ -1,9 +1,17 @@
 package com.little.xmall.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.little.xmall.entity.UserInfo;
+
 /**
  * 用户信息Mapper
  * @author Little
  */
 
-public class UserInfoMapper {
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+    UserInfo findById(Integer userId);
+
+    void update(UserInfo userInfo);
+
+    void delete(int userId);
 }
--- src/main/java/com/little/xmall/service/UserInfoService.java
+++ src/main/java/com/little/xmall/service/UserInfoService.java
@@ -1,9 +1,35 @@
 package com.little.xmall.service;
 
+import com.little.xmall.constant.Response;
+import com.little.xmall.entity.UserInfo;
+import com.little.xmall.constant.Response;
+import java.util.Map;
+import java.util.List;
+
 /**
  * 用户信息Service接口
  * @author Little
  */
 
 public interface UserInfoService {
+    Response<Map<String, Object>> apply(UserInfo userInfo);
+    /**
+     * 创建新的用户信息
+     * @param userInfo 用户信息
+     * @return 创建后的用户信息
+     */
+    UserInfo createUser(UserInfo userInfo);
+
+    /**
+     * 根据用户ID获取用户信息
+     * @param userId 用户ID
+     * @return 用户信息
+     */
+    UserInfo getUserById(Integer userId);
+
+
+    UserInfo updateUser(UserInfo userInfo);
+    void deleteUser(int userId);
+    List<UserInfo> getUsersByCriteria(Map<String, Object> criteria);
+
 }
